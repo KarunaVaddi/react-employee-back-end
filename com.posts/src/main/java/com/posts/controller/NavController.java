1feecaf3736b80b0d00dd9ed332c81b9584d38fc
KarunaVaddi <vaddi.karuna12@gmail.com>
2024-09-19 13:03:43 +0530

    My posts commit

---
+++ com.posts/src/main/java/com/posts/controller/NavController.java
@@ -0,0 +1,25 @@
+package com.posts.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.posts.entities.Post;
+
+@Controller
+public class NavController {
+	@GetMapping("/")
+	public String newPost() {
+		return "newPost";
+	}
+	 @GetMapping("/createPost")
+	    public String createPost() {
+	        return "createPost";
+	    }
+	 
+		
+		
+}
